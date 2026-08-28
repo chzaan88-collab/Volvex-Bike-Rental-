d327d89e0817588a3c9b9fb313d23b8e6118d674
chzaan88-collab <chzaan88@gmail.com>
2026-08-28 21:03:40 +0500

    Add fastapi base_url config in services.php

--- frontend/config/services.php
+++ frontend/config/services.php
@@ -2,24 +2,15 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'postmark' => [
-        'key' => env('POSTMARK_API_KEY'),
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
     ],
 
-    'resend' => [
-        'key' => env('RESEND_API_KEY'),
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
 
     'ses' => [
@@ -28,15 +19,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
-
+    // YE WALI ENTRY CLEAR MISSING HAI ISAY ADD KAREIN:
     'fastapi' => [
-        'base_url' => env('FASTAPI_BASE_URL', env('API_BASE_URL', 'https://volvex-bike-rental.onrender.com/api/v1')),
+        'base_url' => env('API_BASE_URL', 'https://volvex-bike-rental.onrender.com/api/v1'),
     ],
 
 ];
